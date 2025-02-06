ac4f3707e18b1bb7fefb6590d167a174166e92d6
maheshxsharmask <maheshsharmask123@gmail.com>
2025-02-07 00:45:55 +0530

    personal

--- frontend/src/dashboard/resume/components/forms/PersonalDetail.jsx
+++ frontend/src/dashboard/resume/components/forms/PersonalDetail.jsx
@@ -116,42 +116,52 @@
             <div className='p-5 shadow-lg rounded-lg border-t-primary border-t-4 mt-10'>
                 <h2 className='font-bold text-lg'>Personal Detail</h2>
                 <p>Get Started with the basic information</p>
-
+        
                 <form onSubmit={onSave}>
                     <div className='grid grid-cols-2 mt-5 gap-3'>
                         <div>
                             <label className='text-sm'>First Name</label>
-                            <Input name="firstName" value={formData.personalDetails.firstName} required onChange={handleInputChange} />
+                            <Input name="firstName" defaultValue={resumeInfo?.firstName} required onChange={handleInputChange}  />
                         </div>
                         <div>
                             <label className='text-sm'>Last Name</label>
-                            <Input name="lastName" required onChange={handleInputChange} value={formData.personalDetails.lastName} />
+                            <Input name="lastName" required onChange={handleInputChange} 
+                            defaultValue={resumeInfo?.lastName} />
                         </div>
                         <div className='col-span-2'>
                             <label className='text-sm'>Job Title</label>
-                            <Input name="jobTitle" required value={formData.personalDetails.jobTitle} onChange={handleInputChange} />
+                            <Input name="jobTitle" required 
+                            defaultValue={resumeInfo?.jobTitle}
+                            onChange={handleInputChange}  />
                         </div>
                         <div className='col-span-2'>
                             <label className='text-sm'>Address</label>
-                            <Input name="address" required value={formData.personalDetails.address} onChange={handleInputChange} />
+                            <Input name="address" required 
+                            defaultValue={resumeInfo?.address}
+                            onChange={handleInputChange}  />
                         </div>
                         <div>
                             <label className='text-sm'>Phone</label>
-                            <Input name="phone" required value={formData.personalDetails.phone} onChange={handleInputChange} />
+                            <Input name="phone" required 
+                            defaultValue={resumeInfo?.phone}
+                            onChange={handleInputChange}  />
                         </div>
                         <div>
                             <label className='text-sm'>Email</label>
-                            <Input name="email" required value={formData.personalDetails.email} onChange={handleInputChange} />
+                            <Input name="email" required 
+                            defaultValue={resumeInfo?.email}
+                            onChange={handleInputChange}  />
                         </div>
                     </div>
                     <div className='mt-3 flex justify-end'>
-                        <Button type="submit" disabled={loading}>
-                            {loading ? <LoaderCircle className='animate-spin' /> : 'Save'}
-                        </Button>
+                        <Button type="submit"
+                        disabled={loading}>
+                            {loading?<LoaderCircle className='animate-spin' />:'Save'}
+                            </Button>
                     </div>
                 </form>
             </div>
-        );
+          );
     }
 
     export default PersonalDetail;
